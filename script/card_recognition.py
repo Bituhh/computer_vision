f58bd877646bac16737a76b1b82f49d8806a2466
bituhh <victor@oliveira.org.uk>
2018-11-22 13:44:11 +0000

    card warped into a separate image

--- script/card_recognition.py
+++ script/card_recognition.py
@@ -1,31 +1,42 @@
 import cv2
 import numpy as np
 
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(1)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 min_area = 2000
 max_area = 120000
 
+def getContour(img, kernel_shape):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, kernel_shape, 0)
+    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2, 2) 
+    erode = cv2.erode(thresh, kernel, iterations = 1)
+    canny = cv2.Canny(blur, blur.mean()*0.01, blur.mean()*1.5)
+    img, contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+    return contours
+
 
 while cam.isOpened():
     ret, frame = cam.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2, 2)
-    #canny = cv2.Canny(blur, blur.mean()*0.01, blur.mean()*1.5)
-    img, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
-    #cv2.drawContours(frame, contours, -1, (255, 0, 0), 3) 
-    for cnt in contours:        
-            area = cv2.contourArea(cnt)
-            approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
-            if area > min_area and area < max_area and len(approx) is 4:    
-                cv2.drawContours(frame, cnt, -1, (0, 255, 0), 3)
-
-
-    #closing = cv2.morphologyEx(img, cv2.MORPH_CROSS, kernel)
+    contours = getContour(frame, (9, 9))
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
+        if area > min_area and area < max_area and len(approx) is 4:    
+            pts1 = np.float32([approx[3, 0], approx[0, 0], approx[2, 0], approx[1, 0]])
+            pts2 = np.float32([[0,0],[400, 0], [0, 600], [400, 600]])
+            matrix = cv2.getPerspectiveTransform(pts1, pts2)
+            result = cv2.warpPerspective(frame, matrix, (400, 600))
+            #img, cnts, hierarchy = cv2.findContours(, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+            #cnts = cv2.find
+            #convex = cv2.convexHull()
+            
+            cv2.imshow('show', result)
+            cv2.drawContours(frame, cnt, -1, (255, 0, 0), 3)
     
     cv2.imshow('contours', frame)
-    cv2.imshow('thresh', thresh)
+    #cv2.imshow('thresh', closing)
+    #cv2.imshow('canny', erode)
     if cv2.waitKey(1) & 0xFF == ord('x'):
         break
 
