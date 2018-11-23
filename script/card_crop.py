4650d609721e235b72745f30e053e43d232a0fb8
bituhh <victor@oliveira.org.uk>
2018-11-23 00:17:26 +0000

    refactoring to add functions and extracting cards

--- script/card_crop.py
+++ script/card_crop.py
@@ -5,7 +5,7 @@ cam = cv2.VideoCapture(1)
 min_area = 2000
 max_area = 120000
 
-def getContour(img, kernel_shape):
+def getFeatures(img, kernel_shape):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel_shape)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.bilateralFilter(gray,11,17,17)
@@ -19,31 +19,41 @@ def getContour(img, kernel_shape):
     for contour in contours:
         area = cv2.contourArea(contour)
         perimeter = 0.1*cv2.arcLength(contour,True)
-        approx = cv2.approxPolyDP(contour,perimeter,True)
-        if area > min_area and area < max_area and len(approx) is 4:        
+        corner = cv2.approxPolyDP(contour,perimeter,True)
+        if area > min_area and area < max_area and len(corner) is 4:        
             sqr_cnts.append(contour)
-            corners.append(approx)
+            corners.append(corner)
     return sqr_cnts, corners
 
+def extractCard(corner):
+    a_dis = abs(corner[0, 0, 0] - corner[1, 0, 0]) + abs(corner[0 , 0, 1] - corner[1, 0, 1])
+    b_dis = abs(corner[1, 0, 0] - corner[2, 0, 0]) + abs(corner[1 , 0, 1] - corner[2, 0, 1])
+    if a_dis > b_dis:
+        pts1 = np.float32([corner[3, 0], corner[0, 0], corner[2, 0], corner[1, 0]])
+    else:
+        pts1 = np.float32([corner[0, 0], corner[1, 0], corner[3, 0], corner[2, 0]])
+    pts2 = np.float32([[0,0],[200, 0], [0, 300], [200, 300]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    return cv2.warpPerspective(img, matrix, (200, 300))
+
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
 
 while cam.isOpened():
-    ret, frame = cam.read()
-    contours, corners = getContour(frame, (9, 9))
+    ret, img = cam.read()
+    contours, corners = getFeatures(img, (9, 9))
+    cv2.drawContours(img, contours, -1, (255, 0 ,0), 3)
     if len(corners) is not 0:
-        approx = corners[-1]
-        a_dis = abs(approx[0, 0, 0] - approx[1, 0, 0]) + abs(approx[0 , 0, 1] - approx[1, 0, 1])
-        b_dis = abs(approx[1, 0, 0] - approx[2, 0, 0]) + abs(approx[1 , 0, 1] - approx[2, 0, 1])
-        if a_dis > b_dis:
-            pts1 = np.float32([approx[3, 0], approx[0, 0], approx[2, 0], approx[1, 0]])
-        else:
-            pts1 = np.float32([approx[0, 0], approx[1, 0], approx[3, 0], approx[2, 0]])
-        pts2 = np.float32([[0,0],[400, 0], [0, 600], [400, 600]])
-        matrix = cv2.getPerspectiveTransform(pts1, pts2)
-        result = cv2.warpPerspective(frame, matrix, (400, 600))
-    cv2.imshow('show', result)
-    cv2.imshow('contours', frame)
-    #cv2.imshow('thresh', closing)
-    #cv2.imshow('canny', erode)
+        corner = corners[-1]
+        card = extractCard(corner)
+        card_gray = cv2.cvtColor(card, cv2.COLOR_BGR2GRAY)
+        card_gray = cv2.bilateralFilter(card_gray,11,17,17)
+        card_thresh = cv2.adaptiveThreshold(card_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2, 2)
+        #card_opening = cv2.morphologyEx(card_thresh, cv2.MORPH_OPEN, kernel)
+
+        cv2.imshow('card', card)
+        cv2.imshow('thresh', card_thresh[5:75, 5:35])
+
     if cv2.waitKey(1) & 0xFF == ord('x'):
         break
 
