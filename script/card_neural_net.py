401db6b3d6842d665fe7d27b8eede28396f47ae8
bituhh <victor@oliveira.org.uk>
2018-11-29 20:44:49 +0000

    code refactored, saving to move function to cards_utils.py

--- script/card_neural_net.py
+++ script/card_neural_net.py
@@ -1,7 +1,5 @@
 from card_utils import Cards
 from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 import numpy as np
 import cv2
 from random import randint, choice
@@ -9,71 +7,59 @@ from random import randint, choice
 x = 30
 y = 70
 
-#scaler = StandardScaler()
-
-def train():
+def build_dataset(x, y):
     print('Building dataset array...')
+    
+    # Creating a dataset and label lists.
+    
     suits = ['h', 's', 'd', 'c']
     imgs = []
     labels = []
     
-
-
-    # creating a training list from the dataset and label.
     for suit in suits:
         for rank in range(1, 14):
             for card_index in range(250):
                 label = str(rank) + suit
                 img = cv2.imread('../images/' + str(card_index) + '-' + label + '.png', 0)
                 img = cv2.resize(img, (x, y))
-                #thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2, 2)
-                #cv2.imshow('img', img)
-                #cv2.waitKey(0)
                 labels.append(label)
-
-                imgs.append(img)#cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-
+                imgs.append(img)
+ 
+    # Reshape the image into a vector array.
     
-
-    
-    # reshape the image into a vector array. shape = (2600 ,2100)
-    #print('Reshaping and applying PCA to image array! ...')
     reshaped_imgs = np.array(imgs).reshape(len(imgs), -1)
-    #scaler.fit(reshaped_imgs)
-    #imgs = scaler.transform(reshaped_imgs)
-    #mean, eigenVectors = cv2.PCACompute(reshaped_imgs, mean=None, maxComponents=10)
-    #reshaped_imgs = eigenVectors.reshape(-1, len(eigenVectors))
-    #kernel=1.0 * gaussian_process.kernels.RBF(length_scale=10.0)
-    #print(eigenVectors.shape)
-    print(reshaped_imgs.shape)
-    print('Dataset array complete!') 
-    
+    print('Dataset array complete!')
+    return reshaped_imgs, labels
+ 
+def train():
     print('Building MLPClassifier ...')
-    # initialising and training NN
+    
     clf = MLPClassifier(hidden_layer_sizes=(100, 100), random_state=1, batch_size=500, learning_rate_init=0.01, verbose=True, n_iter_no_change=100, shuffle=True)
+    
     try:
         print('Attempting to load pre-trained coeffiencts...')
         coefs = np.load('coefs_.npy')
+        
         if str(input('Coefficients found! Would you like to load it? yes/no')) == 'yes':
             clf.coefs_ = coefs
             print('Coefficients loaded successfully!')
         else:
+            imgs, labels = build_dataset(x, y)
             print('Training MLPClassifier...')
-            clf.fit(reshaped_imgs, labels)
+            clf.fit(imgs, labels)
             print('Training Complete!')
+    
     except:
         print('No pre-trained biases and coeffiencients found!')
         print('Training MLPClassifier...')
         clf.fit(reshaped_imgs, labels)
         print('Training Complete!')
+    
     print('MLPClassifier successfully built!')
-
-    return clf, clf.get_params(True), imgs, labels
+    return clf, clf.get_params(True)
 
 if __name__ == '__main__':
-    
-    
-    clf, params, imgs, labels = train()
+    clf, params = train()
     cam = cv2.VideoCapture(0)
 
     while cam.isOpened():
