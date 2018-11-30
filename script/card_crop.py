e87b4803e359b0512b69cb8448ae56dac1844f0d
bituhh <victor@oliveira.org.uk>
2018-11-30 02:30:08 +0000

    Refactoring, commenting and rearranging folder!

--- script/card_crop.py
+++ script/card_crop.py
@@ -2,45 +2,50 @@ import cv2
 import numpy as np
 from card_utils import Cards
 
-cam = cv2.VideoCapture(0)
-min_area = 2000
-max_area = 120000
+# This code allow you to extract the suit and rank from a image.
+# This was used to build a dataset from scratch. Image is extracted as thresholded
+# images and saves in ../images folder. with label in the format of 6-4d (index-RankSuit)
 
-save = False
-label = 0
+if __name__ == '__main__':i
+    cam = cv2.VideoCapture(0)
+    save = False
+    label = 0
 
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
 
-while cam.isOpened():
-    ret, img = cam.read()
-    cards = Cards(img)
-    corners, contours, hierarchy = cards.getFeatures((9, 9))
-    cv2.drawContours(img, contours, -1, (255, 0 ,0), 3)
-    for corner in corners:
-        card = cards.extractCard(corner)
-        card_gray = cv2.cvtColor(card, cv2.COLOR_BGR2GRAY)
-        card_gray = cv2.bilateralFilter(card_gray,11,17,17)
-        card_thresh = cv2.adaptiveThreshold(card_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2, 2)
-        cv2.imshow('card', card)
-        cv2.imshow('Rank & Suit', card_thresh[7:77, 5:35])
-    cv2.imshow('Camera Image', img)
-    
-    if save is True and label is not 250:
-        cv2.imwrite('../images/' + str(label) + '-' + card_name + '.png', card_thresh[7:77, 5:35])
-        label = label + 1
-    else:
-        save = False
-        label = 0
+    while cam.isOpened():
+        ret, img = cam.read()
+        cards = Cards(img)
+        corners, contours, hierarchy = cards.getFeatures((9, 9))
+        cv2.drawContours(img, contours, -1, (255, 0 ,0), 3)
+        for corner in corners:
+            card = cards.extractCard(corner)
+            card_gray = cv2.cvtColor(card, cv2.COLOR_BGR2GRAY)
+            card_gray = cv2.bilateralFilter(card_gray,11,17,17)
+            card_thresh = cv2.adaptiveThreshold(card_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2, 2)
+            cv2.imshow('card', card)
+            cv2.imshow('Rank & Suit', card_thresh[7:77, 5:35])
+        cv2.imshow('Camera Image', img)
         
-    if label is 249:
-        print('ready for next card!')
-        
-    if cv2.waitKey(1) & 0xFF == ord('x'):
-        if input('what do you wish to do? ') == 'p':
-            card_name = str(input('Which card to save: '))
-            save = True       
+        if save is True and label is not 250:
+            cv2.imwrite('../images/' + str(label) + '-' + card_name + '.png', card_thresh[7:77, 5:35])
+            label = label + 1
         else:
-            break
+            save = False
+            label = 0
+            
+        if label is 249:
+            print('ready for next card!')
+            
+        # By pressing 'o' and option menu will prompt. 
+        # 'c' will crop image and save to ../image folder.
+        # anything else will exit the program.
+        if cv2.waitKey(1) & 0xFF == ord('o'):
+            if input('what do you wish to do? ') == 'p':
+                card_name = str(input('Which card to save: '))
+                save = True       
+            else:
+                break
 
-cam.release()
-cv2.destroyAllWindows()
+    cam.release()
+    cv2.destroyAllWindows()
--- script/card_neural_net.py
+++ script/card_neural_net.py
@@ -3,41 +3,54 @@ import numpy as np
 import cv2
 from random import randint, choice
 
-x = 30
-y = 70
+
 
 if __name__ == '__main__':
+    # Image size. Do not change! Unless you deleted both coefs_.npy and intercepts_.npy file.
+    x = 30
+    y = 70
+
     nn = NeuralNet(x, y)
     clf = nn.train()
+    # Set cv2.VideoCapture parameter to 1 if using USB webcam. Recommended!!
     cam = cv2.VideoCapture(0)
 
     while cam.isOpened():
         ret, img = cam.read()
         cards = Cards(img)
         corners, contours, hierarchy = cards.getFeatures((9, 9))
+        # Extracting a preprocced image containing both suits and rank of the card.
         suits = cards.extractSuits(corners)
-        for suit in suits:
-            suit = cv2.resize(suit, (x, y))
+        for i in range(len(suits)):
+            suit = cv2.resize(suits[i], (x, y))
             prediction = clf.predict(suit.reshape(1, x*y))
+            cv2.putText(img, prediction[0], (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 5)
             print(prediction)
             cv2.imshow('suit', suit)
         prediction = []
         cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
         cv2.imshow('cam', img)
-
+        
+        # Pressing 'o' (options) in the 'Camera Image' window a UI option menu will show at the terminal.
         if cv2.waitKey(1) & 0xFF == ord('o'):
             option = str(input('What would you like to do? '))
-            if option is 't':
-                #retrain
-                nn.retrain(suit.reshape(1, x*y))
-                pass
-            elif option is 'x':
+            # Train option, unfortunatily once trained it looks at that suit & rank only. Not working!!!
+            if option == 'train':
+                # retrain dataset with specific label! Runs but classifications is no longer accurate
+                print(suit.shape)
+                cv2.imshow('Unknown Card!', suit)
+                target = str(input('What card is this?'))
+                nn.retrain(suit.reshape(1, x*y), np.array([target]))
+            elif option == 'exit':
                 # Quit program
                 break
-
-    if str(input('Would you like to save the coefficients? yes/no ')) == 'yes':
-            np.save('coefs_', clf.coefs_)
-            print('Coeffients save in coefs_.npy')
+    
+    # Saving Coefficients and Intercepts values UI. Becarefull not to overfit with this.
+    if str(input('Would you like to save the coefficients & intercepts? yes/no ')) == 'yes':
+            np.save('../data/coefs_', clf.coefs_)
+            np.save('../data/intercepts_', clf.intercepts_)
+            print('Coeffients save in ../data/coefs_.npy')
+            print('Intercepts save in ../data/intercepts_.npy')
 
     cam.release()
     cv2.destroyAllWindows()
